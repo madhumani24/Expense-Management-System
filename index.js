d260d533ef68a4716a20121562639042c26c0090
Madhumani <chakalimadhumani94@gmail.com>
2026-06-25 15:25:28 +0530

    added comments in index.html , index.css , index.js files

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+//to hide threelines menu
 document.getElementById("threelines").style.display="none";
+//appear threelines menu
 let menuopen1 = function(){
     document.querySelector("#threelines").style.display="block";
 }
+//hide threedots menu
 document.querySelector(".threedots").style.display="none";
+//appear threedots menu
 let menuopen2 = function(){
     document.querySelector(".threedots").style.display="block";
     document.getElementById("threelines").style.display="none";
 }
+//close button
 let closebtn = function(){
     document.querySelector("#threelines").style.display="none";
     document.querySelector(".threedots").style.display="none";
